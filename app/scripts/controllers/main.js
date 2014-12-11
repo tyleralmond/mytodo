5e31e190df1fdf4481b84af6000a83abef730e94
Tyler Almond <tyler@tamedia.ca>
2014-12-10 21:32:17 -0500

    Setup the todos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
   });
