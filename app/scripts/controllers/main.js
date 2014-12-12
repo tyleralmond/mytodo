bfe51f367d60817e2e24d7b2335f7988ab3d8ab6
Tyler Almond <tyler@tamedia.ca>
2014-12-11 21:21:45 -0500

    Added in delete and sortable

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,11 @@
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.addTodo = function () {
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+    	$scope.todos.splice(index, 1);
+    };
   });
